8571daf5d5c4e3fad8378ef9ff6837dd75d16c62
LUAN BRENDON <luan_brendonmacedo@hotmail.com>
2023-06-01 20:56:57 -0300

    Mudancas foram feitas (melhorar interface)

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -1,174 +1,158 @@
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.*;
 
 public class ClientMain {
-    private static FastFoodService fastFoodService;
-    private static boolean loggedIn;
-    private static JTextArea itemListTextArea;
-    private static List<String> selectedItems;
-    private static double totalAmount;
+    private static List<String> selectedItems = new ArrayList<>();
+    private static double totalAmount = 0.0;
+    private static String clientName;
 
     public static void main(String[] args) {
         try {
+            // Localizar o registro RMI
             Registry registry = LocateRegistry.getRegistry("localhost", 4444);
-            fastFoodService = (FastFoodService) registry.lookup("FastFoodService");
 
-            JFrame frame = new JFrame("Fast Food - Sistema de Pedidos");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(400, 400);
-            frame.setLayout(new BorderLayout());
+            // Obter a referência do serviço remoto
+            FastFoodService fastFoodService = (FastFoodService) registry.lookup("FastFoodService");
 
-            JPanel loginPanel = new JPanel();
-            loginPanel.setLayout(new GridLayout(3, 2));
+            // Login do cliente
+            login(fastFoodService);
 
-            JLabel usernameLabel = new JLabel("Nome de usuário:");
-            JTextField usernameField = new JTextField();
+            // Loop principal do cliente
+            boolean exitApp = false;
+            while (!exitApp) {
+                // Menu principal
+                String[] options = {"Selecionar item", "Ver itens selecionados", "Fechar pedido", "Sair do aplicativo"};
+                int choice = JOptionPane.showOptionDialog(null, "Selecione uma opção:", "Fast Food App", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
 
-            JLabel passwordLabel = new JLabel("Senha:");
-            JPasswordField passwordField = new JPasswordField();
-
-            JButton loginButton = new JButton("Login");
-            loginButton.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    String username = usernameField.getText();
-                    String password = new String(passwordField.getPassword());
-
-                    try {
-                        loggedIn = fastFoodService.login(username, password);
-                    } catch (RemoteException ex) {
-                        throw new RuntimeException(ex);
-                    }
-
-                    if (loggedIn) {
-                        JOptionPane.showMessageDialog(frame, "Login bem-sucedido!");
-                        selectedItems = new ArrayList<>();
-                        totalAmount = 0.0;
-                    } else {
-                        JOptionPane.showMessageDialog(frame, "Falha no login. Verifique suas credenciais.");
-                    }
+                switch (choice) {
+                    case 0:
+                        // Selecionar item
+                        selectItem(fastFoodService);
+                        break;
+                    case 1:
+                        // Ver itens selecionados
+                        viewSelectedItems();
+                        break;
+                    case 2:
+                        // Fechar pedido
+                        closeOrder(fastFoodService);
+                        break;
+                    case 3:
+                        // Sair do aplicativo
+                        exitApp = true;
+                        break;
+                    default:
+                        // Opção inválida
+                        JOptionPane.showMessageDialog(null, "Opção inválida. Por favor, selecione uma opção válida.");
+                        break;
                 }
-            });
-
-            loginPanel.add(usernameLabel);
-            loginPanel.add(usernameField);
-            loginPanel.add(passwordLabel);
-            loginPanel.add(passwordField);
-            loginPanel.add(loginButton);
-
-            JPanel itemPanel = new JPanel();
-            itemPanel.setLayout(new GridLayout(3, 2));
-
-            JButton cheeseBurgerButton = createItemButton("Cheese Burger", 10.0);
-            JButton xBurgerButton = createItemButton("X-Burger", 12.0);
-            JButton xTudoButton = createItemButton("X-Tudo", 15.0);
-            JButton xAlcatraButton = createItemButton("X-Alcatra", 13.0);
-            JButton batataFritaButton = createItemButton("Batata Frita", 6.0);
-            JButton xCamaraoButton = createItemButton("X-Camarão", 18.0);
-            JButton xSaladaButton = createItemButton("X-Salada", 11.0);
-
-            itemPanel.add(cheeseBurgerButton);
-            itemPanel.add(xBurgerButton);
-            itemPanel.add(xTudoButton);
-            itemPanel.add(xAlcatraButton);
-            itemPanel.add(batataFritaButton);
-            itemPanel.add(xCamaraoButton);
-            itemPanel.add(xSaladaButton);
-
-            JPanel controlPanel = new JPanel();
-            controlPanel.setLayout(new FlowLayout());
-
-            JButton checkoutButton = new JButton("Fechar Pedido");
-            checkoutButton.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    if (!loggedIn) {
-                        JOptionPane.showMessageDialog(frame, "Faça login antes de fechar o pedido.");
-                        return;
-                    }
-
-                    if (selectedItems.isEmpty()) {
-                        JOptionPane.showMessageDialog(frame, "Seu pedido está vazio. Adicione itens antes de fechar o pedido.");
-                        return;
-                    }
-
-                    double total = 0.0;
-                    try {
-                        total = fastFoodService.checkout();
-                        JOptionPane.showMessageDialog(frame, "Pedido fechado! Total a pagar: R$ " + total);
-                        int option = JOptionPane.showConfirmDialog(frame, "Deseja fazer o pagamento?", "Pagamento", JOptionPane.YES_NO_OPTION);
-                        if (option == JOptionPane.YES_OPTION) {
-                            performPayment(total);
-                        }
-                        int exitOption = JOptionPane.showConfirmDialog(frame, "Deseja sair do aplicativo?", "Sair", JOptionPane.YES_NO_OPTION);
-                        if (exitOption == JOptionPane.YES_OPTION) {
-                            System.exit(0);
-                        } else {
-                            clearSelection();
-                        }
-                    } catch (Exception ex) {
-                        JOptionPane.showMessageDialog(frame, "Erro ao fechar o pedido: " + ex.getMessage());
-                    }
-                }
-            });
-
-            controlPanel.add(checkoutButton);
-
-            frame.add(loginPanel, BorderLayout.NORTH);
-            frame.add(itemPanel, BorderLayout.CENTER);
-            frame.add(controlPanel, BorderLayout.SOUTH);
-            frame.setVisible(true);
+            }
         } catch (Exception e) {
-            System.err.println("Erro no cliente: " + e.toString());
             e.printStackTrace();
         }
     }
 
-    private static JButton createItemButton(String itemName, double price) {
-        JButton itemButton = new JButton(itemName + " - R$ " + price);
-        itemButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (!loggedIn) {
-                    JOptionPane.showMessageDialog(null, "Faça login antes de adicionar itens ao pedido.");
-                    return;
-                }
+    private static void login(FastFoodService fastFoodService) throws RemoteException {
+        boolean validLogin = false;
+        while (!validLogin) {
+            String name = JOptionPane.showInputDialog("Digite seu nome de usuário:");
+            String password = JOptionPane.showInputDialog("Digite sua senha:");
 
+            try {
+                validLogin = fastFoodService.login(name, password);
+                if (!validLogin) {
+                    JOptionPane.showMessageDialog(null, "Nome de usuário ou senha inválidos. Por favor, tente novamente.");
+                } else {
+                    clientName = name;
+                }
+            } catch (RemoteException e) {
+                System.out.println("erro" + e.getMessage());
+                JOptionPane.showMessageDialog(null, "Erro durante o login: " + e.getMessage());
+            }
+        }
+    }
+
+    private static void selectItem(FastFoodService fastFoodService) throws RemoteException {
+        String[] options = {"Cheese Burger - R$ 10.00", "X-Burger - R$ 12.00", "X-Tudo - R$ 15.00", "X-Alcatra - R$ 14.00", "Batata Frita - R$ 8.00", "X-Camarão - R$ 18.00", "X-Salada - R$ 9.00"};
+        int choice = JOptionPane.showOptionDialog(null, "Selecione um item:", "Fast Food App - Seleção de Item", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+
+        switch (choice) {
+            case 0:
+                selectedItems.add("Cheese Burger");
+                totalAmount += fastFoodService.getPrice("Cheese Burger");
+                break;
+            case 1:
+                selectedItems.add("X-Burger");
+                totalAmount += fastFoodService.getPrice("X-Burger");
+                break;
+            case 2:
+                selectedItems.add("X-Tudo");
+                totalAmount += fastFoodService.getPrice("X-Tudo");
+                break;
+            case 3:
+                selectedItems.add("X-Alcatra");
+                totalAmount += fastFoodService.getPrice("X-Alcatra");
+                break;
+            case 4:
+                selectedItems.add("Batata Frita");
+                totalAmount += fastFoodService.getPrice("Batata Frita");
+                break;
+            case 5:
+                selectedItems.add("X-Camarão");
+                totalAmount += fastFoodService.getPrice("X-Camarão");
+                break;
+            case 6:
+                selectedItems.add("X-Salada");
+                totalAmount += fastFoodService.getPrice("X-Salada");
+                break;
+            default:
+                // Opção inválida
+                JOptionPane.showMessageDialog(null, "Opção inválida. Por favor, selecione uma opção válida.");
+                break;
+        }
+    }
+
+    private static void viewSelectedItems() {
+        if (selectedItems.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Nenhum item selecionado.");
+        } else {
+            StringBuilder itemList = new StringBuilder();
+            for (String item : selectedItems) {
+                itemList.append(item).append("\n");
+            }
+            JOptionPane.showMessageDialog(null, "Itens selecionados:\n" + itemList);
+        }
+    }
+
+    private static void closeOrder(FastFoodService fastFoodService) throws RemoteException {
+        if (selectedItems.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Nenhum item selecionado. Por favor, selecione pelo menos um item antes de fechar o pedido.");
+        } else {
+            // Exibir o valor total e perguntar se o cliente deseja finalizar o pedido
+            int confirm = JOptionPane.showConfirmDialog(null, "Valor total: R$ " + totalAmount + "\nDeseja finalizar o pedido?", "Confirmação de Pedido", JOptionPane.YES_NO_OPTION);
+            if (confirm == JOptionPane.YES_OPTION) {
                 try {
-                    fastFoodService.addItem(itemName);
-                    selectedItems.add(itemName);
-                    totalAmount += price;
-                    updateItemListTextArea();
-                    JOptionPane.showMessageDialog(null, "Item adicionado ao pedido!");
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(null, "Erro ao adicionar o item: " + ex.getMessage());
+                    // Realizar o pagamento
+                    double amountPaid = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor a ser pago:"));
+                    double change = fastFoodService.processPayment(clientName, amountPaid);
+
+                    // Exibir o troco
+                   // JOptionPane.showMessageDialog(null, "Troco: R$ " + change);
+
+                    // Reiniciar a lista de itens selecionados e o valor total
+//                    selectedItems.clear();
+//                    totalAmount = 0.0;
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Valor pago inválido. Certifique-se de digitar um valor numérico válido.");
+                } catch (RemoteException e) {
+                    System.out.println("erro" + e.getMessage());
+                    JOptionPane.showMessageDialog(null, "Erro durante o processamento do pagamento: " + e.getMessage());
                 }
             }
-        });
-        return itemButton;
-    }
-
-    private static void performPayment(double totalAmount) {
-        // Lógica para realizar o pagamento
-        JOptionPane.showMessageDialog(null, "Pagamento realizado com sucesso!");
-    }
-
-    private static void updateItemListTextArea() {
-        StringBuilder itemList = new StringBuilder();
-        for (String item : selectedItems) {
-            itemList.append(item).append("\n");
         }
-        itemList.append("\nTotal: R$ ").append(totalAmount);
-        itemListTextArea.setText(itemList.toString());
-    }
-
-    private static void clearSelection() {
-        selectedItems.clear();
-        totalAmount = 0.0;
-        updateItemListTextArea();
     }
 }
