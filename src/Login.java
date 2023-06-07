a878b6e31428491bef0ffa8c8de35ae484b9960e
Thor <102772895+thorcamara@users.noreply.github.com>
2023-06-06 22:40:51 -0300

    Adicionei Tela de Login com Array
    Adicionei Tela de Login com Array

---
+++ src/Login.java
@@ -0,0 +1,11 @@
+
+public class Login {
+    public static void main(String[] args) {
+        
+        LoginClient janelaLogin = new LoginClient();
+        janelaLogin.setVisible(true);
+        janelaLogin.setLocationRelativeTo(null);
+        
+        
+    }
+}
